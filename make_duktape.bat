386f108ada144244793797908d87f749269e08cc
huliangjie <lin_032@hotmail.com>
2019-02-07 09:40:10 +0800

    msbuild

--- make_duktape.bat
+++ make_duktape.bat
@@ -6,10 +6,12 @@ mkdir x86
 
 pushd x64
 cmake -G "Visual Studio 15 2017 Win64" ..\..
+msbuild duktape.sln /p:Configuration=Release
 popd
 
 pushd x86
 cmake -G "Visual Studio 15 2017" ..\..
+msbuild duktape.sln /p:Configuration=Release
 popd
 
 popd
