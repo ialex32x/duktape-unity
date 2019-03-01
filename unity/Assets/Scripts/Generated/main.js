be032f89416faba91c6cdaf8acda5fc328ed2cb1
huliangjie <lin_032@hotmail.com>
2019-03-01 21:17:29 +0800

    scratch

--- unity/Assets/Scripts/Generated/main.js
+++ unity/Assets/Scripts/Generated/main.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var foo_1 = require("./mm/foo");
+var vector3_1 = require("./experimental/vector3");
 var foo = new foo_1.Foo(12);
 foo.greet();
 // UnityEngine.Debug.Log("greeting")
@@ -48,49 +49,8 @@ console.log("decodeURIComponent?:", decodeURIComponent);
 // DuktapeJS.Delegate.on(this, (a: string) => {
 //     console.log(a)
 // })
-var Vector3 = function (x, y, z) {
-    this.push(x);
-    this.push(y);
-    this.push(z);
-};
-Vector3.prototype = Object.setPrototypeOf({}, Array).prototype;
-Vector3.prototype.toString = function () {
-    return "test";
-};
-Object.defineProperties(Vector3.prototype, {
-    "normalized": {
-        get: function () {
-            var rlen = 1 / Math.sqrt(this[0] * this[0] + this[1] * this[1] + this[2] * this[2]);
-            return new Vector3(this[0] * rlen, this[1] * rlen, this[2] * rlen);
-        }
-    },
-    "x": {
-        get: function () {
-            return this[0];
-        },
-        set: function (v) {
-            this[0] = v;
-        }
-    },
-    "y": {
-        get: function () {
-            return this[1];
-        },
-        set: function (v) {
-            this[1] = v;
-        }
-    },
-    "z": {
-        get: function () {
-            return this[2];
-        },
-        set: function (v) {
-            this[2] = v;
-        }
-    }
-});
 var scyy = new SampleClass("scyy");
-var v3 = new Vector3(1, 2, 3);
+var v3 = new vector3_1.Vector3(1, 2, 3);
 scyy.TestVector3(v3);
 console.log(v3.x, v3.y, v3.z);
 //# sourceMappingURL=main.js.map
