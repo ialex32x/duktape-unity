d2edd6119c73fbc3dffe18b8a24c3b32741f0e36
huliangjie <lin_032@hotmail.com>
2019-04-08 14:22:12 +0800

    update

--- make_duktape_scratch.bat
+++ make_duktape_scratch.bat
@@ -1,30 +1,9 @@
-pushd duktape-2.3.0
-python .\tools\configure.py --source-directory src-input --output-directory src-scratch --config-metadata config -DDUK_USE_FATAL_HANDLER
-popd
+REM pushd duktape-2.3.0
+REM python .\tools\configure.py --source-directory src-input --output-directory src-scratch --config-metadata config -DDUK_USE_FATAL_HANDLER
+REM popd
 
 mkdir build
 pushd build
-
-REM mkdir librws
-REM pushd librws
-REM rd /s /q x64
-REM mkdir x64
-REM pushd x64
-REM cmake -G "Visual Studio 15 2017 Win64" ..\..\..\librws
-REM popd
-REM cmake --build x64 --config Release
-REM xcopy /Y .\x64\Release\librws_static.lib .\
-REM popd 
-
-REM mkdir duktape
-REM pushd duktape
-REM rd /s /q x64
-REM mkdir x64
-REM pushd x64
-REM cmake -G "Visual Studio 15 2017 Win64" ..\..\..
-REM popd
-REM cmake --build x64 --config Release
-REM popd
 
 mkdir duktape_scratch
 pushd duktape_scratch 
