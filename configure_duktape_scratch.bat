0421d110623d10d1aea4ec7f251d14b02da5d5c9
huliangjie <lin_032@hotmail.com>
2019-06-19 11:18:02 +0800

    recompile duktape source with support for fileName property of Function object

--- configure_duktape_scratch.bat
+++ configure_duktape_scratch.bat
@@ -1,3 +1,3 @@
 pushd duktape-2.3.0
-python .\tools\configure.py --source-directory src-input --output-directory src-scratch --config-metadata config -DDUK_USE_FATAL_HANDLER
+python .\tools\configure.py --source-directory src-input --output-directory src-scratch --config-metadata config -DDUK_USE_FATAL_HANDLER -DDUK_USE_FUNC_FILENAME_PROPERTY
 popd
