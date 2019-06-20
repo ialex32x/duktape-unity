94bf40763ee8581ad4fd431d3e346f9af8681a9b
huliangjie <lin_032@hotmail.com>
2019-06-20 11:48:59 +0800

    circle.example cw/ccw rotation

--- unity/Assets/Examples/Scripts/out/circle.js
+++ unity/Assets/Examples/Scripts/out/circle.js
@@ -8,8 +8,10 @@ var Vector3 = UnityEngine.Vector3;
 var Quaternion = UnityEngine.Quaternion;
 var Time = UnityEngine.Time;
 var cube = GameObject.Find("/Cube");
-var root = new GameObject("cube instances");
-root.transform.localPosition = Vector3.zero;
+var root_cw = new GameObject("cube instances cw");
+root_cw.transform.localPosition = Vector3.zero;
+var root_ccw = new GameObject("cube instances ccw");
+root_ccw.transform.localPosition = Vector3.zero;
 var MyBridge = /** @class */ (function () {
     function MyBridge() {
         this.rot = 0;
@@ -24,13 +26,19 @@ var MyBridge = /** @class */ (function () {
         // copy.transform.localPosition = UnityExtensions.Vector3Rot(up, Quaternion.Euler(0, 0, 45))
         for (var i = 0; i < secs; i++) {
             var slice = i * 360 / secs;
-            var copy = UObject.Instantiate(cube, root.transform);
+            var copy = UObject.Instantiate(cube, root_cw.transform);
+            copy.transform.localPosition = UnityExtensions.Vector3Rot(up, Quaternion.Euler(0, 0, slice));
+        }
+        for (var i = 0; i < secs; i++) {
+            var slice = i * 360 / secs;
+            var copy = UObject.Instantiate(cube, root_ccw.transform);
             copy.transform.localPosition = UnityExtensions.Vector3Rot(up, Quaternion.Euler(0, 0, slice));
         }
     };
     MyBridge.prototype.Update = function () {
         this.rot += Time.deltaTime * 50;
-        root.transform.localRotation = Quaternion.Euler(0, 0, this.rot);
+        root_cw.transform.localRotation = Quaternion.Euler(0, 0, this.rot);
+        root_ccw.transform.localRotation = Quaternion.Euler(0, 0, -this.rot);
     };
     return MyBridge;
 }());
