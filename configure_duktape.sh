517fa8002f74821dbfda9c82ead6814dada01071
huliangjie <lin_032@hotmail.com>
2019-11-29 09:19:00 +0800

    python2 (on 10.15)

--- configure_duktape.sh
+++ configure_duktape.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 cd duktape-2.4.0
-python ./tools/configure.py --source-directory src-input --output-directory src-custom --config-metadata config --dll -DDUK_USE_FATAL_HANDLER -DDUK_USE_FUNC_FILENAME_PROPERTY
+python2 ./tools/configure.py --source-directory src-input --output-directory src-custom --config-metadata config --dll -DDUK_USE_FATAL_HANDLER -DDUK_USE_FUNC_FILENAME_PROPERTY
